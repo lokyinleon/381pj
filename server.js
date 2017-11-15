13d525ebe286c46805d739be63d35ec736c91303
Hoi Ki Wong <s023241@yahoo.com.hk>
2017-11-15 16:52:43 +0800

    Added mongodb related command

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 var formidable = require('formidable');
+var http = require('http');
+var url = require('url');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var mongourl = 'mongodb://hoiki:password@ds141514.mlab.com:41514/hoikitest';
 
 app.set('view engine', 'ejs');
 
@@ -19,40 +25,50 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/auth", function(req, res) {
-    var userid = req.query.name;
-    var password = req.query.password;
+	var userid = req.query.name;
+	var password = req.query.password;
 
-    //query the database to check password
-    //if ok: cookie, redirect to /read
-    //if not ok: redirect to / with error message
+	//query the database to check password
+	//if ok: cookie, redirect to /read
+	//if not ok: redirect to / with error message
 
-    var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
-        var register_userid = fields.registerName;
-        var register_password = fields.registerPassword;
-        console.log("ID:" + register_userid + " Password:" + register_password);
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+	    var register_userid = fields.registerName;
+	    var register_password = fields.registerPassword;
+	    console.log("ID:" + register_userid + " Password:" + register_password);
 
-        if (!register_userid || !register_password) {
-            console.log("PK la you!")
-            var error = "Please fill in both fields.";
-            res.redirect('/register?errorMessage=' + error);
-        } else {
-            console.log("OK");
-            //database operation
-            // if userid == unique, insert userid and password to mongodb
-            // if database return OK! -> redirect /read
-        }
-    });
+	    if (!register_userid || !register_password) {
+	        console.log("PK la you!")
+	        var error = "Please fill in both fields.";
+	        res.redirect('/register?errorMessage=' + error);
+	    } else {
+	        console.log("OK");
+	        //database operation
+	        // if userid == unique, insert userid and password to mongodb
+	        // if database return OK! -> redirect /read
+	        MongoClient.connect(monogourl, function(err, db) {
+	            assert.equal(err, null);
+	            db.collection('users').findOne({ user_id: register_userid }, function(err, result) {
+	                if (result) {
+	                    //This user id found in database
+	                    //user id not unique, not insert
+	                    var error = "This username has been used. Try another user name."
+	                    res.redirect('/register?errorMessage=' + error);
+	                } else {
+	                    //userid is unique
+	                    //do insert 
+	                    console.log("insert");
+	                }
+	                //if ok: redirect to /
+					//if not ok: redirect to /register with error message
 
+	            });
+	        });
 
-    //2 fields must be filled
+	    }
+	});
 
-    //user id must be unique
-
-
-    //query the database to check if existing userid
-    //if ok: cookie, redirect to /
-    //if not ok: redirect to /register with error message
 });
 
 
