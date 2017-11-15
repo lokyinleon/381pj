5bc184f1d43047a07de5f259f6e11bd8648c54ad
Hoi Ki Wong <s023241@yahoo.com.hk>
2017-11-15 15:34:21 +0800

    Change /login to /auth and editted server.js

--- server.js
+++ server.js
@@ -8,4 +8,36 @@ app.get("/", function(req,res) {
 	res.render("sign_in");	
 });
 
+app.get("/register",function(req,res){
+	res.status(200);
+	res.render("sign_up")
+});
+
+app.get("/auth", function(req,res) {
+	var userid = req.query.name;
+	var password = req.query.password;
+
+	//query the database to check password
+	//if ok: cookie, redirect to /read
+	//if not ok: redirect to / with error message
+
+	var register_userid = req.query.registerName;
+	var register_password =  req.query.registerPassword;
+
+	
+
+	//2 fields must be filled
+	//user id must be unique
+
+	//query the database to check if existing userid
+	//if ok: cookie, redirect to /
+	//if not ok: redirect to /register with error message
+});
+
+
+app.get("/read", function(req,res) {
+	res.status(200);
+	res.render("read", {});	
+});
+
 app.listen(process.env.PORT || 8099);
