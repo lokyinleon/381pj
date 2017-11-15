9a7da7160ac1fb59403b062018066f2a1e15cf93
Hoi Ki Wong <s023241@yahoo.com.hk>
2017-11-15 16:33:06 +0800

    milestone for register validation

--- server.js
+++ server.js
@@ -1,43 +1,64 @@
 var express = require('express');
 var app = express();
+var formidable = require('formidable');
 
 app.set('view engine', 'ejs');
 
-app.get("/", function(req,res) {
-	res.status(200);
-	res.render("login");	
+app.get("/", function(req, res) {
+    res.status(200);
+    res.render("login");
 });
 
-app.get("/register",function(req,res){
-	res.status(200);
-	res.render("register")
+app.get("/register", function(req, res) {
+    res.status(200);
+    var err = req.query.errorMessage;
+    if (!req.query.errorMessage)
+        err = "";
+    res.render("register", { errorMessage: err });
+    //res.render("register", {errorMessage: '1'})
 });
 
-app.get("/auth", function(req,res) {
-	var userid = req.query.name;
-	var password = req.query.password;
+app.post("/auth", function(req, res) {
+    var userid = req.query.name;
+    var password = req.query.password;
 
-	//query the database to check password
-	//if ok: cookie, redirect to /read
-	//if not ok: redirect to / with error message
+    //query the database to check password
+    //if ok: cookie, redirect to /read
+    //if not ok: redirect to / with error message
 
-	var register_userid = req.query.registerName;
-	var register_password =  req.query.registerPassword;
-    console.log("ID:"+register_userid+"Password:"+register_password);
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        var register_userid = fields.registerName;
+        var register_password = fields.registerPassword;
+        console.log("ID:" + register_userid + " Password:" + register_password);
+
+        if (!register_userid || !register_password) {
+            console.log("PK la you!")
+            var error = "Please fill in both fields.";
+            res.redirect('/register?errorMessage=' + error);
+        } else {
+            console.log("OK");
+            //database operation
+            // if userid == unique, insert userid and password to mongodb
+            // if database return OK! -> redirect /read
+        }
+    });
 
 
-	//2 fields must be filled
-	//user id must be unique
+    //2 fields must be filled
 
-	//query the database to check if existing userid
-	//if ok: cookie, redirect to /
-	//if not ok: redirect to /register with error message
+    //user id must be unique
+
+
+    //query the database to check if existing userid
+    //if ok: cookie, redirect to /
+    //if not ok: redirect to /register with error message
 });
 
 
-app.get("/read", function(req,res) {
-	res.status(200);
-	res.render("read", {});	
+app.get("/read", function(req, res) {
+    res.status(200);
+    res.render("read", {});
 });
 
 app.listen(process.env.PORT || 8099);
