d0c7ee6060db1819dd95e12418b37907b17e6c9f
Hoi Ki Wong <s023241@yahoo.com.hk>
2017-11-15 18:10:33 +0800

    Login logic

--- server.js
+++ server.js
@@ -15,16 +15,51 @@ app.get("/", function(req, res) {
     var msg = req.query.message;
     if (!msg)
         msg = "";
-    res.render("login", {message: msg});
+    res.render("login", { message: msg });
 });
 
-app.post("/login_auth", function(req, res){
-	var userid = req.query.name;
-	var password = req.query.password;
+app.post("/login_auth", function(req, res) {
 
-	//query the database to check password
-	//if ok: cookie, redirect to /read
-	//if not ok: redirect to / with error message
+
+    //query the database to check password
+    //if ok: cookie, redirect to /read
+    //if not ok: redirect to / with error message
+
+
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        var userid = fields.name;
+        var password = fields.password;
+
+        if (!userid || !password) {
+            var error = "Please enter both fields.";
+            res.redirect('/?message=' + error);
+        } else {
+            MongoClient.connect(mongourl, function(err, db) {
+                assert.equal(err, null);
+                db.collection('users').findOne({ user_id: userid }, function(err, result) {
+                    if (result) {
+                        //have this user, check password
+                        if (result.password == password) {
+                            console.log("Login sucess");
+                            res.redirect('/read')
+                        } else {
+                            console.log("Password not match");
+                            var msg = "Password not match";
+                            res.redirect('/?message=' + msg);
+                        }
+                    } else {
+                        //dont have this user (userid/password error)
+                        console.log("No this user");
+                        var msg = "No this user";
+                        res.redirect('/?message=' + msg);
+                    }
+
+                });
+            });
+        }
+    });
+
 });
 
 app.get("/register", function(req, res) {
@@ -37,60 +72,60 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/reg_auth", function(req, res) {
-	var form = new formidable.IncomingForm();
-	form.parse(req, function(err, fields, files) {
-	    var register_userid = fields.registerName;
-	    var register_password = fields.registerPassword;
-	    console.log("ID:" + register_userid + " Password:" + register_password);
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        var register_userid = fields.registerName;
+        var register_password = fields.registerPassword;
+        console.log("ID:" + register_userid + " Password:" + register_password);
 
-	    if (!register_userid || !register_password) {
-	        console.log("PK la you!")
-	        var error = "Please fill in both fields.";
-	        res.redirect('/register?errorMessage=' + error);
-	    } else {
-	        console.log("OK");
-	        //database operation
-	        // if userid == unique, insert userid and password to mongodb
-	        // if database return OK! -> redirect /read
-	        MongoClient.connect(mongourl, function(err, db) {
-	            assert.equal(err, null);
-	            db.collection('users').findOne({ user_id: register_userid }, function(err, result) {
-	                 assert.equal(err, null);
-	                if (result) {
-	                    //This user id found in database
-	                    //user id not unique, not insert
-	                    var error = "This username has been used. Try another user name."
-	                    res.redirect('/register?errorMessage=' + error);
-	                } else {
-	                    //userid is unique
-	                    //do insert 
-	                    console.log("insert");
-	                    db.collection('users').insertOne({user_id:register_userid,password:register_password},function(err,result) {
-		                assert.equal(err,null);
-		                console.log("Insert was successful!");
-		                console.log(JSON.stringify(result[0]));
-		                var message = "";
-		                if(err){
-                        	message = "Account creation error";
-		                } else{
-		                	message = "Your account have been created";
-		                }
-		                console.log(message);
-		                res.redirect('/?message='+message);
-	});
-	                }
-	            });
-	        });
+        if (!register_userid || !register_password) {
+            var error = "Please fill in both fields.";
+            res.redirect('/register?errorMessage=' + error);
+        } else {
+            console.log("OK");
+            //database operation
+            // if userid == unique, insert userid and password to mongodb
+            // if database return OK! -> redirect /read
+            MongoClient.connect(mongourl, function(err, db) {
+                assert.equal(err, null);
+                db.collection('users').findOne({ user_id: register_userid }, function(err, result) {
+                    assert.equal(err, null);
+                    if (result) {
+                        //This user id found in database
+                        //user id not unique, not insert
+                        var error = "This username has been used. Try another user name."
+                        res.redirect('/register?errorMessage=' + error);
+                    } else {
+                        //userid is unique
+                        //do insert 
+                        console.log("insert");
+                        db.collection('users').insertOne({ user_id: register_userid, password: register_password }, function(err, result) {
+                            assert.equal(err, null);
+                            console.log("Insert was successful!");
+                            console.log(JSON.stringify(result[0]));
+                            var message = "";
+                            if (err) {
+                                message = "Account creation error";
+                            } else {
+                                message = "Your account have been created";
+                            }
+                            console.log(message);
+                            res.redirect('/?message=' + message);
+                        });
+                    }
+                });
+            });
 
-	    }
-	});
+        }
+    });
 
 });
 
 
 app.get("/read", function(req, res) {
     res.status(200);
-    res.render("read", {});
+    res.end("read");
+    //res.render("read", {});
 });
 
 app.listen(process.env.PORT || 8099);
